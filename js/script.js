f4622a08982db283a93ec65908412b8c3e9a5dac
Caterina <danizio@outlook.it>
2020-11-18 15:50:34 +0100

    Logica inserimento toDo item in lista

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// DESCRIZIONE:
+// Creiamo una mini app di todolist con html+css+js in Vue
+// Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+// Istanza Vue Js
+var app = new Vue({
+  el: '#app',
+  data: {
+    logo: "https://img.icons8.com/clouds/2x/todo-list.png",
+    item: "",
+    list: [],
+},
+
+  methods: {
+       add: function() {
+         this.list.push(this.item);
+       }
+     },
+});
